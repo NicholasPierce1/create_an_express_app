1e9ee090e2257ede026b2e093ee9b285bebad69c
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-03-26 09:36:10 -0500

    finished app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { siteTitle: 'This Awesome Site!', name : "Nick" });
 });
 
 module.exports = router;

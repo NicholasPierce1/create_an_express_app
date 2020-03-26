7893ad0edc644727e47328c5a5ee99a247ee08af
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-03-26 09:40:29 -0500

    fixed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { siteTitle: 'This Awesome Site!', name : "Nick" });
+  res.render('index', { siteTitle: 'This Awesome Site!', name : "Nick", title: "Welcome to express!!"});
 });
 
 module.exports = router;
